7d0314ebe2284305ebe51a731d4b5ba5c544164c
Manoj <manoj.delhi24@gmail.com>
2018-12-02 20:57:14 +0700

    Actor replying to other actor

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -1,14 +1,16 @@
 package part2actors
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 object ActorCapabilities extends App {
 
   class SimpleActor extends Actor {
     def receive: Receive = {
-      case message: String => println(s"[simple actor] received: $message")
+      case "Hello" => context.sender() ! "Hey, there! " // replying to a message
+      case message: String => println(s"[simple actor $self] received: $message")
       case number: Int => println(s"[simple actor] received int: $number")
       case SpecialMessage(content) => println(s"[simple actor] received SpecialMessage: $content")
+      case SayHelloTo(actorRef) => actorRef ! "Hello"
     }
   }
 
@@ -22,4 +24,32 @@ object ActorCapabilities extends App {
 
   case class SpecialMessage(msg: String)
   simpleActor ! SpecialMessage("this is special object!")
+
+  /*
+    We can send any messages with 2 conditions
+    a. messages must be IMMUTBABLE
+    b. messages must be SERIALIZABLE - JVM should be able to convert it to byte code and send to another JVM
+    Always remember these principles.
+  */
+
+  /*
+    2. Actors have information about their context and about themselves.
+       Each actor has a member called context, its a complex data-structure that has references to
+       information regarding the environment this actor is running.
+       Ex:
+        - it has access to the actor system (`context.system`)
+        - it has access to this actor's own actor reference (`context.self`)
+          - we can use self ref to send a messages to ourselves.
+          `context.self === this`
+  */
+
+
+  //3. How actors can REPLY to the messages
+
+  val tom = actorSystem.actorOf(Props[SimpleActor], "tom")
+  val jerry = actorSystem.actorOf(Props[SimpleActor], "jerry")
+  case class SayHelloTo(ref: ActorRef)
+  tom ! SayHelloTo(jerry)
+
+
 }
