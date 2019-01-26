a0a73ef2284db7d5e2253a16faea4419631d86f4
Manoj <manoj.delhi24@gmail.com>
2019-01-26 17:22:20 +0700

    implicit and explicit logging in actor system

---
+++ src/main/scala/part2actors/ActorLoggingDemo.scala
@@ -0,0 +1,31 @@
+package part2actors
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.event.Logging
+
+object ActorLoggingDemo extends App {
+
+  class SimpleAcotor extends Actor {
+    val logger = Logging(context.system, this)
+
+    def receive: Receive = {
+      case message => logger.info("[explicit logging]: {}", message)
+    }
+  }
+  val system = ActorSystem("LoggingDemo")
+  val simpleAcotor = system.actorOf(Props[SimpleAcotor])
+  simpleAcotor ! "Msg sample"
+
+  class ActorWithLogging extends Actor with ActorLogging {
+    def receive: Receive = {
+      case message => log.info("[implicit logging]: {}", message)
+    }
+  }
+
+  val anotherAcotor = system.actorOf(Props[ActorWithLogging])
+  anotherAcotor ! "Another msg sample"
+
+
+  Thread.sleep(1000)
+  system.terminate
+}
