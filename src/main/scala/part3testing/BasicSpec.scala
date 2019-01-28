dc7128e3616c38694e953cff0654434a67c7672f
Manoj <manoj.delhi24@gmail.com>
2019-01-28 08:58:32 +0700

    Akka test case added to check async messages, any message and all messages. Also used reply from testActor

--- src/main/scala/part3testing/BasicSpec.scala
+++ src/main/scala/part3testing/BasicSpec.scala
@@ -1,8 +1,11 @@
 package part3testing
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+
+import scala.concurrent.duration._
+import scala.util.Random
 
 class BasicSpec extends TestKit(ActorSystem("basicSpec"))
   with ImplicitSender
@@ -24,9 +27,71 @@ class BasicSpec extends TestKit(ActorSystem("basicSpec"))
     }
   }
 
+  import BasicSpec._
+
+  // how to do async assertions
+  "A SimpleActor" should {
+    "send back the same message" in {
+      val simpleActor = system.actorOf(Props[SimpleActor])
+      val msg = "Hey actor"
+      simpleActor ! msg
+
+      expectMsg(msg) // akka.test.single-expect-default
+    }
+  }
+
+  "A Blackhole" should {
+    "send back the same message" in {
+      val sallowActor = system.actorOf(Props[Blackhole])
+      val msg = "Hey actor"
+      sallowActor ! msg
+
+      expectNoMessage(1 second)
+    }
+  }
+
+  "A lab test actor" should {
+    val labActor = system.actorOf(Props[LabActor])
+
+    "turn string into uppercase" in {
+      labActor ! "Hey actor"
+      val reply = expectMsgType[String]
+
+      assert(reply == "HEY ACTOR")
+    }
+
+    "reply (randomly) to a greeting" in {
+      labActor ! "Greeting"
+      expectMsgAnyOf("Hi", "Hello")
+    }
+
+    "reply with fav tech" in {
+      labActor ! "favTech"
+      expectMsgAllOf("Scala", "Akka")
+    }
+  }
 }
 
 // Its recommended to create a companion object, here you store all the methods or all the values that you're going to use in your tests
 object BasicSpec {
+  class SimpleActor extends Actor {
+    def receive: Receive = {
+      case msg => sender ! msg
+    }
+  }
 
+  class Blackhole extends Actor {
+    def receive: Receive = Actor.emptyBehavior
+  }
+
+  class LabActor extends Actor {
+    val random = new Random()
+    def receive: Receive = {
+      case "Greeting" => sender ! (if (random.nextBoolean()) "Hello" else "Hi")
+      case "favTech" =>
+        sender ! "Akka"
+        sender ! "Scala"
+      case msg: String => sender ! msg.toUpperCase
+    }
+  }
 }
