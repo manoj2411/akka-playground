21d2c0db12d3f3842d17946d28b6f9f37b52c5d0
Manoj <manoj.delhi24@gmail.com>
2019-04-11 21:56:36 +0700

    creating selfClosing actor which stops iteslf if doesn;t receive message for long time.

--- src/main/scala/part5infra/TimersSchedulers.scala
+++ src/main/scala/part5infra/TimersSchedulers.scala
@@ -1,6 +1,7 @@
 package part5infra
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Cancellable, Props}
+
 import scala.concurrent.duration._
 
 object TimersSchedulers extends App {
@@ -19,22 +20,59 @@ object TimersSchedulers extends App {
   val system = ActorSystem("TimersSchedulersDemo")
   val simpleActor = system.actorOf(Props[SimpleActor])
 
-  system.log.info("Scheduling reminder!")
+//  system.log.info("Scheduling reminder!")
   import system.dispatcher
 
   system.scheduler.scheduleOnce(1 second) {
-    simpleActor ! "gentle reminder!"
+//    simpleActor ! "gentle reminder!"
   } // Scheduling of code has to happen on some threat much like futures. So we need an execution context.
 
 
   // Scheduling a repeating message
   val repeating = system.scheduler.schedule(1 second, 2 seconds) {
-    simpleActor ! "heartbeat"
+//    simpleActor ! "heartbeat"
   }
 
-  Thread.sleep(5000)
+//  Thread.sleep(5000)
   repeating.cancel()
 
-  //Thread.sleep(2000)
+
+  /* Exercise: Implement a self closing actor
+  - If actor receives a message then you 1 second to send it another message
+  - Otherwise actor will stop itself
+  - If another messages comes then the time window resets
+  */
+
+  class SelfClosingActor extends Actor with ActorLogging {
+    var lastScheduled = schedule
+
+    def schedule: Cancellable =   context.system.scheduler.scheduleOnce( 1 second ) {
+      self ! "stop"
+    }
+
+    def receive: Receive = {
+      case "stop" =>
+        context.stop(self)
+      case message =>
+        log.info(s"[message]: $message")
+        lastScheduled.cancel()
+        lastScheduled = schedule
+    }
+  }
+
+  val selfClosingActor = system.actorOf(Props[SelfClosingActor])
+  selfClosingActor ! "first message"
+  system.scheduler.scheduleOnce(500 millis ) {
+    selfClosingActor ! "ping"
+  }
+
+  system.scheduler.scheduleOnce(2000 millis ) {
+    selfClosingActor ! "pong"
+  }
+
+
+
+
+  Thread.sleep(2000)
   system.terminate
 }
