4714785e1427e550fe712b88020d31cb2bff166c
Manoj <manoj.delhi24@gmail.com>
2019-04-12 08:33:42 +0700

    Creating actor with timer which can be used to schedule messages to self.

--- src/main/scala/part5infra/TimersSchedulers.scala
+++ src/main/scala/part5infra/TimersSchedulers.scala
@@ -1,6 +1,6 @@
 package part5infra
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Cancellable, Props}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Cancellable, Props, Timers}
 
 import scala.concurrent.duration._
 
@@ -23,18 +23,18 @@ object TimersSchedulers extends App {
 //  system.log.info("Scheduling reminder!")
   import system.dispatcher
 
-  system.scheduler.scheduleOnce(1 second) {
+//  system.scheduler.scheduleOnce(1 second) {
 //    simpleActor ! "gentle reminder!"
-  } // Scheduling of code has to happen on some threat much like futures. So we need an execution context.
+//  } // Scheduling of code has to happen on some threat much like futures. So we need an execution context.
 
 
   // Scheduling a repeating message
-  val repeating = system.scheduler.schedule(1 second, 2 seconds) {
+//  val repeating = system.scheduler.schedule(1 second, 2 seconds) {
 //    simpleActor ! "heartbeat"
-  }
+//  }
 
 //  Thread.sleep(5000)
-  repeating.cancel()
+//  repeating.cancel()
 
 
   /* Exercise: Implement a self closing actor
@@ -61,18 +61,41 @@ object TimersSchedulers extends App {
   }
 
   val selfClosingActor = system.actorOf(Props[SelfClosingActor])
-  selfClosingActor ! "first message"
-  system.scheduler.scheduleOnce(500 millis ) {
-    selfClosingActor ! "ping"
+//  selfClosingActor ! "first message"
+//  system.scheduler.scheduleOnce(500 millis ) {
+//    selfClosingActor ! "ping"
+//  }
+
+//  system.scheduler.scheduleOnce(2000 millis ) {
+//    selfClosingActor ! "pong"
+//  }
+
+
+  // Timers: used to schedule message to self, from within
+
+  val timerKey = "demoTimer"
+  class TimerBasedActor extends Actor with ActorLogging with Timers {
+    timers.startSingleTimer(timerKey, "start", 500 millis)
+
+    def receive: Receive = {
+      case "start" =>
+        log.info("Starting timer actor")
+        timers.startPeriodicTimer(timerKey, "ping", 1 second) // reusing same timerKey since it was Single Timer
+      case "ping" =>
+        log.info("received ping!")
+      case "stop" =>
+        log.warning("Stopping timer actor!")
+        timers.cancel(timerKey)
+        context.stop(self)
+    }
   }
 
-  system.scheduler.scheduleOnce(2000 millis ) {
-    selfClosingActor ! "pong"
+  val timerActor = system.actorOf(Props[TimerBasedActor], "timerActor")
+  system.scheduler.scheduleOnce(5 seconds) {
+    timerActor ! "stop"
   }
 
 
-
-
-  Thread.sleep(2000)
-  system.terminate
+//  Thread.sleep(2000)
+//  system.terminate
 }
