9fbe1c116421a39f98d65a6c073df48bbc464887
Manoj <manoj.delhi24@gmail.com>
2019-10-25 12:32:51 +0530

    correcting indentation

--- src/main/scala/part6patterns/FiniteStateMachineDemo.scala
+++ src/main/scala/part6patterns/FiniteStateMachineDemo.scala
@@ -9,8 +9,8 @@ import scala.concurrent.duration._
 
 
 
-  class VendingMachineSpec extends TestKit(ActorSystem("afmTest"))
-    with ImplicitSender with WordSpecLike with BeforeAndAfterAll {
+class VendingMachineSpec extends TestKit(ActorSystem("afmTest"))
+  with ImplicitSender with WordSpecLike with BeforeAndAfterAll {
 
     override def afterAll(): Unit = {
       TestKit.shutdownActorSystem(system)
@@ -105,77 +105,77 @@ import scala.concurrent.duration._
       }
 
     }
+}
+
+
+object VendingMachine {
+
+  case class Init(prices: Map[String, Int], stock: Map[String, Int])
+  case class RequestItem(item: String)
+
+  case class Instruction(message: String)
+  case class ReceiveMoney(amount: Int)
+  case class Error(reason: String)
+  case class GiveChange(amount: Int)
+
+  case class DeliverItem(item: String)
+  case class Timeout(originalSender: ActorRef)
+}
+
+class VendingMachine extends Actor with ActorLogging {
+  import VendingMachine._
+  implicit val ex: ExecutionContext = context.dispatcher
+
+  def receive: Receive = idle
+
+  def idle: Receive = {
+    case Init(prices, stock) =>
+      log.info("Initializing")
+      context.become(ready(prices, stock))
+    case _ =>
+      sender ! Error("not ready to use")
   }
 
-
-  object VendingMachine {
-    case class Init(prices: Map[String, Int], stock: Map[String, Int])
-    case class RequestItem(item: String)
-
-    case class Instruction(message: String)
-    case class ReceiveMoney(amount: Int)
-    case class Error(reason: String)
-
-    case class GiveChange(amount: Int)
-    case class DeliverItem(item: String)
-    case class Timeout(originalSender: ActorRef)
+  def ready(prices: Map[String, Int], stock: Map[String, Int]): Receive = {
+    case RequestItem(item) => stock.get(item) match {
+      case Some(0) =>
+        sender ! Error("out of stock")
+      case None =>
+        sender ! Error("invalid item")
+      case Some(_) =>
+        val price = prices(item)
+        sender ! Instruction(s"Please insert $price dollars")
+        context.become(waitingForMoney(prices, stock, item, scheduleTimeout(sender), 0))
+    }
   }
 
-  class VendingMachine extends Actor with ActorLogging {
-    import VendingMachine._
-    implicit val ex: ExecutionContext = context.dispatcher
+  def waitingForMoney(prices: Map[String, Int], stock: Map[String, Int], item: String, timeout: Cancellable, amount: Int): Receive = {
+    case ReceiveMoney(currAmount) if amount + currAmount < prices(item) =>
+      timeout.cancel()
+      val moneyReceived = amount + currAmount
+      val remainingMoney = prices(item) - moneyReceived
+      sender ! Instruction(s"Please insert $remainingMoney dollars")
+      context.become(waitingForMoney(prices, stock, item, scheduleTimeout(sender), moneyReceived))
+    case ReceiveMoney(currAmount) =>
+      timeout.cancel()
+      val newStock = stock + (item -> (stock(item) - 1))
+      val receivedMoney = amount + currAmount
+      sender ! DeliverItem(item)
+      if (receivedMoney > prices(item))
+        sender ! GiveChange(receivedMoney - prices(item))
+      context.become(ready(prices, newStock))
+    case Timeout(originalSender) =>
+      log.info("timeout!")
+      if (amount > 0 )
+        originalSender ! GiveChange(amount)
+      originalSender ! Error("timeout")
+      context.become(ready(prices, stock))
+  }
 
-    def receive: Receive = idle
-
-    def idle: Receive = {
-      case Init(prices, stock) =>
-        log.info("Initializing")
-        context.become(ready(prices, stock))
-      case _ =>
-        sender ! Error("not ready to use")
+  private def scheduleTimeout(originalSender: ActorRef): Cancellable =
+    context.system.scheduler.scheduleOnce(1 second) {
+      self ! Timeout(originalSender)
     }
-
-    def ready(prices: Map[String, Int], stock: Map[String, Int]): Receive = {
-      case RequestItem(item) => stock.get(item) match {
-        case Some(0) =>
-          sender ! Error("out of stock")
-        case None =>
-          sender ! Error("invalid item")
-        case Some(_) =>
-          val price = prices(item)
-          sender ! Instruction(s"Please insert $price dollars")
-          context.become(waitingForMoney(prices, stock, item, scheduleTimeout(sender), 0))
-      }
-    }
-
-    def waitingForMoney(prices: Map[String, Int], stock: Map[String, Int], item: String, timeout: Cancellable, amount: Int): Receive = {
-      case ReceiveMoney(currAmount) if amount + currAmount < prices(item) =>
-        timeout.cancel()
-        val moneyReceived = amount + currAmount
-        val remainingMoney = prices(item) - moneyReceived
-        sender ! Instruction(s"Please insert $remainingMoney dollars")
-        context.become(waitingForMoney(prices, stock, item, scheduleTimeout(sender), moneyReceived))
-      case ReceiveMoney(currAmount) =>
-        timeout.cancel()
-        val newStock = stock + (item -> (stock(item) - 1))
-        val receivedMoney = amount + currAmount
-
-        sender ! DeliverItem(item)
-        if (receivedMoney > prices(item))
-          sender ! GiveChange(receivedMoney - prices(item))
-        context.become(ready(prices, newStock))
-      case Timeout(originalSender) =>
-        log.info("timeout!")
-        if (amount > 0 )
-          originalSender ! GiveChange(amount)
-        originalSender ! Error("timeout")
-        context.become(ready(prices, stock))
-    }
-
-    private def scheduleTimeout(originalSender: ActorRef): Cancellable =
-      context.system.scheduler.scheduleOnce(1 second) {
-        self ! Timeout(originalSender)
-      }
 
   }
 
