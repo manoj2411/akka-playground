74be8dfb8659404a21cbd788ac16d9799ec1b3d1
Manoj <manoj.delhi24@gmail.com>
2019-10-26 10:57:14 +0530

    Adding FSM based actor implementation with same functionality as VendingMachine actor.

--- src/main/scala/part6patterns/FiniteStateMachineDemo.scala
+++ src/main/scala/part6patterns/FiniteStateMachineDemo.scala
@@ -15,9 +15,9 @@ class VendingMachineSpec extends TestKit(ActorSystem("afmTest"))
     override def afterAll(): Unit = {
       TestKit.shutdownActorSystem(system)
     }
+    import VendingMachine._
 
     "VendingMachine" should {
-      import VendingMachine._
 
       "send error when not initialised" in {
         val vendingMachine = system.actorOf(Props[VendingMachine])
@@ -105,6 +105,95 @@ class VendingMachineSpec extends TestKit(ActorSystem("afmTest"))
       }
 
     }
+
+  "VendingMachine FSM" should {
+
+    "send error when not initialised" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+      vendingMachine ! RequestItem
+
+      expectMsg(Error("not ready to use"))
+    }
+
+    "report invalid item" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+      vendingMachine ! Init(Map(), Map())
+      vendingMachine ! RequestItem("coke")
+
+      expectMsg(Error("invalid item"))
+    }
+
+    "report item out of stock" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+      vendingMachine ! Init(Map(), Map("coke" -> 0))
+      vendingMachine ! RequestItem("coke")
+
+      expectMsg(Error("out of stock"))
+    }
+
+    "throw timeout after 1 sec when no money received" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+      vendingMachine ! Init(Map("coke" -> 2), Map("coke" -> 2))
+      vendingMachine ! RequestItem("coke")
+      expectMsg(Instruction("Please insert 2 dollars"))
+
+      within(1.5 second) {
+        expectMsg(Error("timeout"))
+      }
+    }
+
+    "receive money back when timeout" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+      vendingMachine ! Init(Map("coke" -> 2), Map("coke" -> 10))
+      vendingMachine ! RequestItem("coke")
+      expectMsg(Instruction("Please insert 2 dollars"))
+      vendingMachine ! ReceiveMoney(1)
+      expectMsg(Instruction("Please insert 1 dollars"))
+
+      within(1.5 second) {
+        expectMsg(GiveChange(1))
+        expectMsg(Error("timeout"))
+      }
+    }
+
+    "receive item and change when send all money" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+
+      vendingMachine ! Init(Map("coke" -> 2), Map("coke" -> 10))
+      vendingMachine ! RequestItem("coke")
+      expectMsg(Instruction("Please insert 2 dollars"))
+      vendingMachine ! ReceiveMoney(2)
+
+      expectMsg(DeliverItem("coke"))
+    }
+
+    "receive item and change when send more money" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+
+      vendingMachine ! Init(Map("coke" -> 2), Map("coke" -> 10))
+      vendingMachine ! RequestItem("coke")
+      expectMsg(Instruction("Please insert 2 dollars"))
+      vendingMachine ! ReceiveMoney(5)
+
+      expectMsg(DeliverItem("coke"))
+      expectMsg(GiveChange(3))
+    }
+
+    "receive item and change when send money by more than 1 message" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+
+      vendingMachine ! Init(Map("coke" -> 3), Map("coke" -> 10))
+      vendingMachine ! RequestItem("coke")
+      expectMsg(Instruction("Please insert 3 dollars"))
+      vendingMachine ! ReceiveMoney(2)
+      expectMsg(Instruction("Please insert 1 dollars"))
+      vendingMachine ! ReceiveMoney(2)
+
+      expectMsg(DeliverItem("coke"))
+      expectMsg(GiveChange(1))
+    }
+
+  }
 }
 
 
@@ -183,7 +272,7 @@ class VendingMachine extends Actor with ActorLogging {
 
 //  states
 trait VendingState
-case object Idle extends VendingState
+case object NotReady extends VendingState
 case object Ready extends VendingState
 case object WaitingForMoney extends VendingState
 
@@ -208,22 +297,72 @@ case class WaitingForMoneyData(prices: Map[String, Int],
 */
 
 class VendingMachineFSM extends FSM[VendingState, VendingData] {
+  import VendingMachine._
 
   // we need to handle States & Events, not the messages
 
-  startWith(Idle, NotReadyData)
+  startWith(NotReady, NotReadyData)
 
-  when(Idle) {
-    ???
+  when(NotReady) {
+    case Event(Init(prices, stock), NotReadyData) =>
+      goto(Ready) using ReadyData(prices, stock)
+      // === context.become(ready(prices, stock))
+    case _ =>
+      sender ! Error("not ready to use")
+      stay()
   }
 
   when(Ready) {
-    ???
+    case Event(RequestItem(item), ReadyData(prices, stock)) => stock.get(item) match {
+      case Some(0) =>
+        sender ! Error("out of stock")
+        stay()
+      case None =>
+        sender ! Error("invalid item")
+        stay()
+      case Some(_) =>
+        val price = prices(item)
+        sender ! Instruction(s"Please insert $price dollars")
+        // context.become(waitingForMoney(prices, stock, item, scheduleTimeout(sender), 0))
+        goto(WaitingForMoney) using WaitingForMoneyData(prices, stock, item, sender, 0)
+    }
+
   }
 
-  when(WaitingForMoney) {
-    ???
+  when(WaitingForMoney, stateTimeout = 1 second) {
+    case Event(StateTimeout, WaitingForMoneyData(prices, stock, item, requester, amount)) =>
+      log.info("timeout!")
+      if (amount > 0 )
+        requester ! GiveChange(amount)
+      requester ! Error("timeout")
+      // context.become(ready(prices, stock))
+      goto(Ready) using ReadyData(prices, stock)
+    case Event(ReceiveMoney(currAmount), WaitingForMoneyData(prices, stock, item, requester, amount)) if amount + currAmount < prices(item) =>
+      val moneyReceived = amount + currAmount
+      val remainingMoney = prices(item) - moneyReceived
+      requester ! Instruction(s"Please insert $remainingMoney dollars")
+      // context.become(waitingForMoney(prices, stock, item, scheduleTimeout(sender), moneyReceived))
+      stay() using part6patterns.WaitingForMoneyData(prices, stock, item, requester, moneyReceived)
+    case Event(ReceiveMoney(currAmount), WaitingForMoneyData(prices, stock, item, requester, amount)) =>
+      val newStock = stock + (item -> (stock(item) - 1))
+      val receivedMoney = amount + currAmount
+      requester ! DeliverItem(item)
+      if (receivedMoney > prices(item))
+        requester ! GiveChange(receivedMoney - prices(item))
+      // context.become(ready(prices, newStock))
+      goto(Ready) using ReadyData(prices, newStock)
   }
 
+  whenUnhandled {
+    case Event(_, _) =>
+      sender() ! Error("command not found")
+      stay()
+  }
+
+  onTransition {
+    case stateA -> stateB => log.info(s"changing from $stateA to $stateB")
+  }
+
+  initialize() // IMP: Starts the chain of handlers
 }
 
